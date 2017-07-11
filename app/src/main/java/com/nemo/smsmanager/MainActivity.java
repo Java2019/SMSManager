4adf995e5d06a23b66724e915f04709779d03c94
java2019 <java_2019@mail.ru>
2017-07-11 10:32:41 +0300

    first step

--- app/src/main/java/com/nemo/smsmanager/MainActivity.java
+++ app/src/main/java/com/nemo/smsmanager/MainActivity.java
@@ -1,7 +1,13 @@
 package com.nemo.smsmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendManager = (Button)findViewById(R.id.sendManager);
+        sendManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendManager();
+            }
+        });
+
+        Button sendIntent = (Button)findViewById(R.id.sendIntent);
+        sendIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendIntent();
+            }
+        });
+    }
+
+    private void sendIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra("address", new String("0123456789"));
+        try {
+            startActivity(intent);
+        }catch (Exception  e){
+
+        }
+    }
+
+    private void sendManager() {
+
+        String phoneNum = "0123456789";
+        String textSms  = "hi";
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNum, null, textSms, null, null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
